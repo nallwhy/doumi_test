b5dd6b66bc3424fef06277baa8b49a46a7828707
Jinkyou Son <nallwhy@gmail.com>
2023-11-10 12:46:01 +0900

    Add same_values?/2 to Doumi.Test

--- lib/doumi/test.ex
+++ lib/doumi/test.ex
@@ -1,18 +1,33 @@
 defmodule Doumi.Test do
-  @moduledoc """
-  Documentation for `Doumi.Test`.
-  """
-
   @doc """
-  Hello world.
+  Compare two values for equality.
 
   ## Examples
+      iex> Doumi.Test.same_values?(1, 1)
+      true
 
-      iex> Doumi.Test.hello()
-      :world
+      iex> Doumi.Test.same_values?("foo", "bar")
+      false
 
+      iex> Doumi.Test.same_values?(~U[2023-01-01 00:00:00Z], ~U[2023-01-01 00:00:00.000Z])
+      true
+
+      iex> Doumi.Test.same_values?(Decimal.new("1.1"), 1.1)
+      true
   """
-  def hello do
-    :world
+  @spec same_values?(any(), any()) :: boolean()
+  def same_values?(%DateTime{} = a, %DateTime{} = b), do: DateTime.compare(a, b) == :eq
+
+  if Code.ensure_loaded?(Decimal) do
+    def same_values?(%Decimal{} = a, %Decimal{} = b), do: Decimal.equal?(a, b)
+    def same_values?(%Decimal{} = a, b) when is_integer(b), do: Decimal.equal?(a, b)
+
+    def same_values?(%Decimal{} = a, b) when is_float(b),
+      do: Decimal.equal?(a, Decimal.from_float(b))
+
+    def same_values?(%Decimal{} = a, b) when is_binary(b), do: Decimal.equal?(a, Decimal.new(b))
+    def same_values?(a, %Decimal{} = b), do: same_values?(b, a)
   end
+
+  def same_values?(a, b), do: a == b
 end
--- test/doumi/test_test.exs
+++ test/doumi/test_test.exs
@@ -1,8 +1,61 @@
 defmodule Doumi.TestTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  alias Doumi.Test
   doctest Doumi.Test
 
-  test "greets the world" do
-    assert Doumi.Test.hello() == :world
+  describe "same_values?/2" do
+    test "returns true comparing two DateTimes with the same value but different precision" do
+      assert Test.same_values?(~U[2023-01-01 00:00:00Z], ~U[2023-01-01 00:00:00.000Z]) == true
+    end
+
+    test "returns false comparing two DateTimes with different values" do
+      assert Test.same_values?(~U[2023-01-01 00:00:00Z], ~U[2023-01-01 00:00:01Z]) == false
+    end
+
+    test "returns true comparing two Decimals with the same value" do
+      assert Test.same_values?(Decimal.new("1.1"), Decimal.new("1.1")) == true
+    end
+
+    test "returns true comparing Decimal and integer with the same value" do
+      assert Test.same_values?(Decimal.new("1"), 1) == true
+      assert Test.same_values?(1, Decimal.new("1")) == true
+    end
+
+    test "returns false comparing Decimal and integer with different values" do
+      assert Test.same_values?(Decimal.new("1"), 2) == false
+      assert Test.same_values?(2, Decimal.new("1")) == false
+    end
+
+    test "returns true comparing Decimal and float with the same value" do
+      assert Test.same_values?(Decimal.new("1.1"), 1.1) == true
+      assert Test.same_values?(1.1, Decimal.new("1.1")) == true
+    end
+
+    test "returns false comparing Decimal and float with different values" do
+      assert Test.same_values?(Decimal.new("1.1"), 1.2) == false
+      assert Test.same_values?(1.2, Decimal.new("1.1")) == false
+    end
+
+    test "returns true comparing Decimal and string with the same value" do
+      assert Test.same_values?(Decimal.new("1.1"), "1.1") == true
+      assert Test.same_values?("1.1", Decimal.new("1.1")) == true
+    end
+
+    test "returns false comparing Decimal and string with different values" do
+      assert Test.same_values?(Decimal.new("1.1"), "1.2") == false
+      assert Test.same_values?("1.2", Decimal.new("1.1")) == false
+    end
+
+    test "returns true comparing the same things" do
+      assert Test.same_values?(1, 1) == true
+      assert Test.same_values?(1.1, 1.1) == true
+      assert Test.same_values?("foo", "foo") == true
+    end
+
+    test "returns false comparing two different things" do
+      assert Test.same_values?(1, 2) == false
+      assert Test.same_values?(1.1, 1.2) == false
+      assert Test.same_values?("foo", "bar") == false
+    end
   end
 end
